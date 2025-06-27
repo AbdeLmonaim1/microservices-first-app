cac00951d39dce951a29a63bc26049b7611a1d2d
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-06-27 20:21:09 +0100

    OrderController class, which handles HTTP requests related to orders. It defines endpoints such as POST /api/order for placing orders. The class uses OrderService to process business logic

--- ordre-service/src/main/java/ma/firstmicroservice/ordreservice/controller/OrderController.java
+++ ordre-service/src/main/java/ma/firstmicroservice/ordreservice/controller/OrderController.java
@@ -1,5 +1,26 @@
 package ma.firstmicroservice.ordreservice.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ma.firstmicroservice.ordreservice.dto.OrderRequest;
+import ma.firstmicroservice.ordreservice.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/order")
+@AllArgsConstructor
+@Slf4j
 public class OrderController {
+    private OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        log.info("Placing Order");
+        return CompletableFuture.supplyAsync(() -> orderService.saveOrder(orderRequest));
+    }
+
 
 }
